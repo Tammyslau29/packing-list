9cd176354dd1f353832f5a24b48675a9a06c9d16
Tammy <tammyslau29@gmail.com>
2017-04-20 07:41:41 -0700

    created form to add new items to database

---
+++ databaseForm.js
@@ -0,0 +1,13 @@
+/**
+ * Created by tammyslau on 4/20/17.
+ */
+$(document).ready(function(){
+    $(document).ready(function() {
+        $('select').material_select();
+    });
+
+})
+function clearForm() {
+    $("#itemName").val("");
+    $("#itemDetail").val("");
+}
