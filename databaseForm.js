a42fa96005991902252b68284d1f35305b348e3f
Tammy <tammyslau29@gmail.com>
2017-04-20 08:16:52 -0700

    ability to push items to different categories in firebase

--- databaseForm.js
+++ databaseForm.js
@@ -20,17 +20,44 @@ function clearForm() {
     $("#itemName").val("");
     $("#category").empty();
     $("#itemDetail").val("");
-}
+};
 function addToDatabase(){
     var database = firebase.database();
+    var dbObject = createDbObject();
     switch ($("#category").val()){
         case "1":
-            database.ref().push({
-                username: name,
-                email: email,
-                profile_picture : imageUrl
-            });
+            database.ref("Clothing/" + dbObject.name + "/").set(dbObject);
+            break;
+        case "2":
+            database.ref("Toiletries/" + dbObject.name + "/").set(dbObject);
+            break;
+        case "3":
+            database.ref("Makeup/" + dbObject.name + "/").set(dbObject);
+            break;
+        case "4":
+            database.ref("Food/" + dbObject.name + "/").set(dbObject);
+            break;
+        case "5":
+            database.ref("Footwear/" + dbObject.name + "/").set(dbObject);
+            break;
+        case "6":
+            database.ref("Paperwork/" + dbObject.name + "/").set(dbObject);
+            break;
+        case "7":
+            database.ref("Gifts/" + dbObject.name + "/").set(dbObject);
+            break;
+        case "8":
+            database.ref("Electronics/" + dbObject.name + "/").set(dbObject);
             break;
     }
-
+    clearForm();
 };
+function createDbObject(){
+    var itemName = $("#itemName").val();
+    var itemDetail = $("#itemDetail").val();
+    var objectGoingToDb = {
+        name: itemName,
+        detail: itemDetail,
+    }
+    return objectGoingToDb
+}
